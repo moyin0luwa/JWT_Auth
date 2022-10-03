0625b8e8d2ce7252b9b80e1ea349a9357ba9e1a8
Moyinoluwalogo <m.oladipsgmail.com>
2022-10-03 20:58:35 +0100

    First commit -Set up folder structure -Install needed dependencies -Set up and ran the server -Infuse Error handling into the server along with creating a custom error -Set up enviromental variables

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+// async errors
+require('express-async-errors')
+
+const express = require("express");
+const app = express();
+
+// const connectDB = require('./src/db/connect')
+
+// const productRoute = require('./src/router/router')
+
+// Error Handling
+const notFoundMiddleware = require("./src/middleware/not-found");
+const errorMiddleware = require("./src/middleware/error-handler");
+
+//express json middleware
+app.use(express.json());
+//middlewarefor the frontend
+app.use(express.static('./public'))
+
+// Route
+// app.use('/products', productRoute )
+// app.get("/", (req, res) => {
+// 	res.send("STORE IS OPEN");
+// });
+
+// using the error handlers
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		// connectDB
+        // await connectDB(process.env.MONGO_URI)
+		app.listen(port, () => console.log(`Server listening on PORT ${port}...`));
+	} catch (error) {
+		console.log(error); 
+	}
+};
+
+start();
