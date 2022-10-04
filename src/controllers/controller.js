6663bf0e9e94570c53f5bdc07f5823ff89b1be81
Moyinoluwalogo <m.oladipsgmail.com>
2022-10-04 21:21:40 +0100

    Fourth commit; -continued setting up of the jwt auth -In particular, created the payload to be signed along with the secret to be used in the signing, Secret as created as an enviromental variable

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,12 +1,23 @@
-const CustomAPIError = require('../errors/custom-error')
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+require("dotenv").config();
 
 const login = async (req, res) => {
 	const { username, password } = req.body;
 	if (!username || !password) {
-		throw new CustomAPIError('Please provide valid credentials', 400)
+		throw new CustomAPIError("Please provide valid credentials", 400);
 	}
 	console.log(username, "|", password);
-	res.send("Fake Login route");
+
+	//if the conditions for the username is fulfilled then we want to create our payload then sign it with the secret
+	const SECRET = process.env.SECRET;
+	const dateTime = new Date().getDate();
+	let payload = {
+		username,
+		dateTime,
+	};
+	const token = jwt.sign(payload, SECRET, { expiresIn: 86400 });
+	res.status(200).json({ msg: "User Logged in", token });
 };
 
 const dashboard = async (req, res) => {
