6aa12da5dfc5ce0237bb47c30e0d8a0869c6b661
Moyinoluwalogo <m.oladipsgmail.com>
2022-10-05 11:27:51 +0100

    Fifth Commit -Completed the process of the JWT Authentication -Set up jwt to verify and decode received token and throw a custom error in the event of an error -Started up on tweaking the code to be more efficient setting up the authorization middleware -Completed the logic behind the front end of the application

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -7,25 +7,37 @@ const login = async (req, res) => {
 	if (!username || !password) {
 		throw new CustomAPIError("Please provide valid credentials", 400);
 	}
-	console.log(username, "|", password);
-
 	//if the conditions for the username is fulfilled then we want to create our payload then sign it with the secret
 	const SECRET = process.env.SECRET;
-	const dateTime = new Date().getDate();
+	const id = new Date().getDate();
 	let payload = {
 		username,
-		dateTime,
+		id,
 	};
 	const token = jwt.sign(payload, SECRET, { expiresIn: 86400 });
 	res.status(200).json({ msg: "User Logged in", token });
 };
 
 const dashboard = async (req, res) => {
-	const secretNumber = Math.floor(Math.random() * 100);
-	res.status(200).json({
-		msg: `Shhhhhh, Want the Secret Number`,
-		secret: `Here it is ${secretNumber}`,
-	});
+	//first we grab the token from the auth header and assign it to a variable
+	const authHeader = req.headers.authorization;
+	//Then we validate the token sent
+	if (!authHeader || !authHeader.startsWith("Bearer ")) {
+		throw new CustomAPIError("Invalid credentials to access this route", 401);
+	}
+	const token = authHeader.split(" ")[1];
+
+	//Verifying if this token is actually valid and then decoding the token to access the info in the payload
+	try {
+		const decoded = jwt.verify(token, process.env.SECRET); //The token here is verified and decoded with the already set secret
+		const secretNumber = Math.floor(Math.random() * 100);
+		res.status(200).json({
+			msg: `Shhhhhh ${decoded.username}, Want the Secret Number?`,
+			secret: `Here it is, ${secretNumber}`,
+		});
+	} catch (error) {
+		throw new CustomAPIError("Not Authorised to access this route", 401);
+	}
 };
 
 module.exports = {
---
+++ src/middleware/auth.js
@@ -0,0 +1,3 @@
+//Authentication Middleware which perfroms the functionality of authencicating a user before giving the user access to the route.
+//The authentication process is basically the validating, verifying and decoding of the token sent
+
